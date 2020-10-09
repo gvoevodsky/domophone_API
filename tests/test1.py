64059ab837696d32f62665138dd0d71d7b1e0018
GVoevodsky <gigagnom13@mail.ru>
2020-10-09 19:10:05 +0300

    recursion in test1.py

--- tests/test1.py
+++ tests/test1.py
@@ -6,10 +6,10 @@ def change_bool(dictionary, key):  # u can play with this func
     if dictionary[key] is True:
         dictionary[key] = False
     elif dictionary[key] is False:
-        dictionary[key] = True
+        dictionary[key] = False
     else:
         print('ERROR', dictionary, key)
-
+    return dictionary[key]
 
 def revert_bool(dictionary_var):  # function go though json with level 3 nesting
     dictionary = dictionary_var  # and changing all bool values with change_bool(dict, key)
@@ -37,29 +37,35 @@ def revert_bool(dictionary_var):  # function go though json with level 3 nesting
             pass
     return dictionary
 
+def send_put_request(io_object, api_menu, data_to_send):
+    request = io_object.send_request(method='PUT', api_menu=api_menu,
+                                     data=json.dumps(data_to_send))
+    print(data_to_send, request.json())
+
+
+def revert_bool_recoursion(io_object, dictionary, keypath=[], api_menu='configuration'):
+    for key, value in dictionary.items():
+        if type(value) is dict:
+            keypath.append(key)
+            revert_bool_recoursion(io_object, dictionary[key], keypath, api_menu)
+            keypath.pop()
+
+        elif type(value) is bool:
+            new_value = change_bool(dictionary, key)
+            fullPath = {key : new_value}
+            for pathKey in keypath[::-1]:
+                fullPath = {pathKey: fullPath}
+            send_put_request(io_object=io_object, api_menu=api_menu, data_to_send=fullPath)
+        else:
+            pass
 
 def test_main(io_object):
     response = io_object.send_request(method='GET', api_menu='configuration', data=None)
-    json_dict = response.json()
-    print('GET №1\n', json_dict)
-    print('\n\n\n')
-    print(response.text)
-    time.sleep(1)
+    json_dict = response.json()['response']
+    print(json_dict)
+    print(type(json_dict))
 
-
-    revert_dict = revert_bool(json_dict)
-    print('reverted\n', revert_dict)
-    time.sleep(1)
-
-    print('sending reverted boolean \n')
-    request = io_object.send_request(method='PUT', api_menu='configuration', data=json.dumps(revert_dict['response']))
-    print(request.json())
-
-    print('GET №2\n')
+    revert_bool_recoursion(io_object, json_dict, api_menu='configuration')
     response = io_object.send_request(method='GET', api_menu='configuration', data=None)
-    print('server response:\n')
-    print(response.json())
-
-
-if __name__ == '__main__':
-    test_main('something') #?
+    json_dict = response.json()
+    print(json_dict)
